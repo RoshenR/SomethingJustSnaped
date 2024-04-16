5df893022c89e2a0e628b40e615ad6c451b2ab6a
RoshenR <crisliath@gmail.com>
2024-04-16 10:06:36 +0200

    Ajout_differents_portails_et_zones_individuel

---
+++ snap-switch/snap-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const gifDisplayDuration = 2500;
+
+    setTimeout(function() {
+        window.location.href = 'Ricardo/ricardo-index.html';
+    }, gifDisplayDuration);
+});
